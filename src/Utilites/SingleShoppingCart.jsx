851c67eaf905e287e22dc0d2381d0119b9b6f288
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-28 22:47:16 +0600

    shopping cart responsive

--- src/Utilites/SingleShoppingCart.jsx
+++ src/Utilites/SingleShoppingCart.jsx
@@ -6,15 +6,17 @@ const SingleShoppingCart = ({
 }) => {
   const { image, quantity, name, price, id } = item;
   return (
-    <div className="grid grid-cols-6 items-center border-1 border-[#e3e4e5] rounded-lg mt-2 px-3 py-4 gap-4">
-      <div className="flex items-center gap-3 col-span-2">
+    <div className="grid lg:grid-cols-6 grid-cols-1 items-center border-1 border-[#e3e4e5] rounded-lg mt-2 px-3 py-4 gap-4">
+      <div className="lg:flex items-center gap-3 lg:col-span-2">
         <img
-          className="w-[93px] h-[93px] rounded-lg object-cover"
+          className="w-[300px] h-[200px] mx-auto lg:mx-0 lg:w-[93px] lg:h-[93px] rounded-lg object-cover"
           src={image}
           alt=""
         />
-        <div>
-          <h3>{name}</h3>
+        <div className="text-center lg:text-left mt-4 lg:mt-0">
+          <h3 className="text-xl font-bold lg:text-[18px] mb-2 lg:mb-0">
+            {name}
+          </h3>
           <p className="text-[#787878]">
             color: <span className="text-black font-bold">White</span>
           </p>
@@ -23,8 +25,10 @@ const SingleShoppingCart = ({
           </p>
         </div>
       </div>
-      <p>{price}</p>
-      <div className="flex items-center">
+      <p className="text-center lg:text-left font-bold lg:font-normal">
+        {price}
+      </p>
+      <div className="flex items-center justify-center lg:justify-start">
         <button
           onClick={() => handleDecreaseQuantity(id)}
           className="border-2 border-gray-300 px-2 py-1 text-xl font-bold cursor-pointer border-r-0 rounded-l-full"
@@ -41,7 +45,9 @@ const SingleShoppingCart = ({
           +
         </button>
       </div>
-      <h3>{(price * quantity).toFixed(2)}</h3>
+      <h3 className="font-bold lg:font-normal text-center lg:text-left">
+        {(price * quantity).toFixed(2)}
+      </h3>
       <button
         onClick={() => handleDeleteButton(id)}
         className="bg-red-500 text-white py-2 rounded-full border-2 border-red-500 hover:bg-transparent hover:text-red-500 duration-500 cursor-pointer"
