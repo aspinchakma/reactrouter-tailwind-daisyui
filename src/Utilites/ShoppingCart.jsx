149c6296d36737efbacc87bc67daaf7695688411
Aspin Chakma <aspinchakma2019@gmail.com>
2025-08-28 16:13:48 +0600

    proceed to checkout button setup

--- src/Utilites/ShoppingCart.jsx
+++ src/Utilites/ShoppingCart.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 import { Bounce, toast } from "react-toastify";
 import { calCulateTotalMoney, loadDataLs } from "./DataLS";
@@ -192,6 +193,10 @@ const ShoppingCart = () => {
                   {(total + 20).toFixed(2)}
                 </p>
               </div>
+              <button className="px-3 py-2 bg-[#f83d8e] text-white font-bold rounded-full flex items-center gap-2 w-full mt-5 justify-center hover:bg-transparent hover:text-[#f83d8e] border-1 border-[#f83d8e] cursor-pointer duration-500">
+                <span>proceed to checkout</span>
+                <FaArrowRight />
+              </button>
             </div>
           </div>
         </div>
